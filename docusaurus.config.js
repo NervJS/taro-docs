4321a492fd5d87ad21f8a01ad7c9288ae86dee71
huangli47 <huangli@jd.com>
2022-10-13 23:41:58 +0800

    chore: logo 地址调整

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -311,7 +311,7 @@ const siteConfig = {
                 }
               },
               output: {
-                publicPath: './'
+                publicPath: '/'
               }
             }
           };
