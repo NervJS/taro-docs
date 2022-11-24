9fc27e67351e8e7a362b821b3f080b7fda214812
Zakary <zakarycode@gmail.com>
2022-11-24 11:37:53 +0800

    feat: 新增 canIUse 导航 & 提示输入

--- src/pages/canIUse/index.js
+++ src/pages/canIUse/index.js
@@ -5,6 +5,18 @@ import Layout from '@theme/Layout';
 import types from 'miniapp-types/dist/jsonSchema.json';
 import './index.css';
 
+const useList = {}
+types.forEach((e) => {
+  const list = e.value || []
+  list.forEach(item => {
+    useList[item?.key] = true
+    const props = item?.value?.properties || {}
+    Object.keys(props).forEach((propName) => {
+      useList[propName] = true
+    })
+  })
+})
+
 function CanIUse() {
   const [search, setSearch] = useState('');
 
@@ -56,9 +68,17 @@ function CanIUse() {
     <Layout permalink="/caniuse">
       <div className="can-i-use-wrap">
         <div className="d-flex align-items-center justify-content-center w-100 mb-5">
-          <label>Can I use</label>
-          <input type="text" value={search} onChange={handleChanges} />
-          <label>?</label>
+          <details>
+            <summary>
+              <label htmlFor="can-i-use">Can I use</label>
+              <input list="use-list" id="can-i-use" name="can-i-use" type="text" value={search} onChange={handleChanges} />
+              <label>?</label>
+            </summary>
+            当前支持<strong>组件库</strong>精确查询，可匹配组件名、属性名
+          </details>
+          <datalist id="use-list">
+            {Object.keys(useList).map((name) => (<option value={name} />))}
+          </datalist>
         </div>
         <div className="w-100">
           {componentName.map((item) => {
