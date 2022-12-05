d6e673d967db1ab0980feb8d8cf7fa02c91e7c15
Zakary <zakarycode@gmail.com>
2022-12-05 17:29:17 +0800

    fix: add eslint with some develop-tools

--- typings/global.d.ts
+++ typings/global.d.ts
@@ -1,4 +1,6 @@
-declare module "*.svg" {
+declare module '*.svg' {
   const SVGProps = React.ComponentProps<React.SVGAttributes<SVGElement>>
   export default React.ComponentType<SVGProps>
 }
+
+declare module '@site/static/*'
